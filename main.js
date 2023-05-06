1db4bc0e2a4a55fdd41428d8cddbaf12dbd8d04f
Rubia Avelar <125077910+rubyavelar@users.noreply.github.com>
2023-05-06 16:37:28 -0300

    criando uma lista de teclas

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
+
+//Pom -------------------------------------------
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play()
-}
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
+listaDeTeclas[0].onclick = tocaSomPom
